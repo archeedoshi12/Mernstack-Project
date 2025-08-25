c3679f234fa5d8f818d17b4c12be27084ab1b859
Archee Doshi <135125083+archeedoshi12@users.noreply.github.com>
2025-08-25 21:40:57 +0530

    Update api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mernstack-1-0tp6.onrender.com",
+  baseURL: "https://mernstack-project-2.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
