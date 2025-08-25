5cf7a2e01f25b0b4f031d653348d98a15ea6f73d
Archee Doshi <archeedoshi12@gmail.com>
2025-08-25 19:23:07 +0530

    change on login api in frontend

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,53 +1,13 @@
-import api from "./api";
+import axios from "axios";
 
-const API_URL = "https://mernstack-1-0tp6.onrender.com/admin/api/projects";
+const API_URL = "https://mernstack-1-0tp6.onrender.com/api/auth";
 
-export const getProjects = async () => {
-  try {
-    const response = await api.get(API_URL);
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching projects:", error);
-    throw error;
-  }
+export const registerUser = async (userData) => {
+  const response = await axios.post(`${API_URL}/register`, userData);
+  return response.data;
 };
 
-export const createProject = async (projectData) => {
-  try {
-    const response = await api.post(API_URL, projectData);
-    return response.data;
-  } catch (error) {
-    console.error("Error creating project:", error);
-    throw error;
-  }
-};
-
-export const updateProject = async (id, updates) => {
-  try {
-    const response = await api.put(`${API_URL}/${id}`, updates);
-    return response.data;
-  } catch (error) {
-    console.error("Error updating project:", error);
-    throw error;
-  }
-};
-
-export const deleteProject = async (id) => {
-  try {
-    const response = await api.delete(`${API_URL}/${id}`);
-    return response.data;
-  } catch (error) {
-    console.error("Error deleting project:", error);
-    throw error;
-  }
-};
-
-export const getMyProjects = async () => {
-  try {
-    const response = await api.get(`${API_URL}/getUserProjects`);
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching user projects:", error);
-    throw error;
-  }
+export const loginUser = async (userData) => {
+  const response = await axios.post(`${API_URL}/login`, userData);
+  return response.data;
 };
